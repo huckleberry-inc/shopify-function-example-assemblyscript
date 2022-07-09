e7d1f9681923793384deb50ce4399e3dfb19a772
camelmasa <camelmasa@gmail.com>
2022-07-10 02:20:30 +0900

    refactor:

--- test/helper.ts
+++ test/helper.ts
@@ -1,11 +1,11 @@
 import { execSync } from 'child_process';
 import { writeFileSync, unlinkSync } from 'fs';
 
-const INPUT_FILE_PATH = "./test/input.json";
 export const functionRunner = (input: string) => {
-  const createInputFile = (input: string) => writeFileSync(INPUT_FILE_PATH, input);
-  const outputByFunctionRunner = () => execSync(`./function-runner -f build/release.wasm ${INPUT_FILE_PATH} -j | jq .output.JsonOutput`)
-  const reset = () => unlinkSync(INPUT_FILE_PATH);
+  const inputJsonFile = "./test/input.json";
+  const createInputFile = (input: string) => writeFileSync(inputJsonFile, input);
+  const outputByFunctionRunner = () => execSync(`./function-runner -f build/release.wasm ${inputJsonFile} -j | jq .output.JsonOutput`)
+  const reset = () => unlinkSync(inputJsonFile);
 
   createInputFile(input);
   const output = outputByFunctionRunner();
